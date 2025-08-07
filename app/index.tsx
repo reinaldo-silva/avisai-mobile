2f9c7fc89fe854fe259378b73191b2ba06062ae9
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2025-08-07 11:25:58 -0300

    authentication struct app

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Home() {
-  return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-lg font-bold">Hello, Expo Router + Tailwind! </Text>
-    </View>
-  );
-}
---
+++ src/app/_layout.tsx
@@ -0,0 +1,28 @@
+import { SessionProvider, useSession } from "@/ctx";
+import { SplashScreenController } from "@/splash";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <SessionProvider>
+      <SplashScreenController />
+      <RootNavigator />
+    </SessionProvider>
+  );
+}
+
+function RootNavigator() {
+  const { session } = useSession();
+
+  return (
+    <Stack>
+      <Stack.Protected guard={!!session}>
+        <Stack.Screen name="(app)" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!session}>
+        <Stack.Screen name="sign-in" />
+      </Stack.Protected>
+    </Stack>
+  );
+}
